3b9b04f91112ffb6837ab286fa56a4325c526e98
Kimber Lynch <kmlynch714@gmail.com>
2016-07-18 17:06:05 -0400

    added custom polarity form

--- extension/content.js
+++ extension/content.js
@@ -1,48 +1,75 @@
 var sentiment = require('sentiment');
 var _ = require('lodash');
 
-var retext = require('retext');
+var Retext = require('retext');
 var inspect = require('unist-util-inspect');
 var retextSentiment = require('retext-sentiment');
 
+var retext = new Retext();
 
+
+//CUSTOM POLARITY SETTINGS
+//Setting
+	//get input as message from popup
+	//send to local chrome storage?
+
+//Retrieving
+	//retrieve stored options from chrome storage
+	chrome.storage.sync.get(null, function (items){
+		for(var key in items) items[key] = Number(items[key]);
+			console.log(items);
+		//apply to retext
+		retext.use(retextSentiment, items);
+		//send to popup.js to be displayed
+	});
+
+chrome.storage.onChanged.addListener(function(changes){
+	console.log('changed!', changes);
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
+  });
+});
+
+//APPLYING/REMOVING BLURS
+//listen for messages to apply/remove blurring effect
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-	if (msg.command && (msg.command == "update_tweet_view")) {
+	if (msg.command && (msg.command == "set_blur")) {
 		blur();
 		$(window).on('scroll', _.debounce(function () {
 			blur();
-		}, 100));
+		}, 500));
 
-	} else if (msg.command && (msg.command == "reset_tweet_view")) {
+	} else if (msg.command && (msg.command == "set_unblur")) {
 		unblur();
 		$(window).off('scroll');
 	}
 });
 
+//call blur function depending on social media site
 function blur() {
 	if (location.href.search(/facebook\.com/) > -1) return fbblur();
-	if (location.href.search(/twitter\.com/) > -1)	return tweetblur();
+	if (location.href.search(/twitter\.com/) > -1) return tweetblur();
 }
 
-function analyzeAndCss(text, parent, children){
-retext.use(retextSentiment).use(function () {
-			return function (tree) {
-				if (tree.data) {
-					score = tree.data.polarity;
-				}
-			};
-		}).process(text);
+function analyzeAndCss(text, parent, children) {
+	retext.use(retextSentiment).use(function () {
+		return function (tree) {
+			if (tree.data) {
+				score = tree.data.polarity;
+			}
+		};
+	}).process(text);
 
-		if (score < 0) {
-			score = (score - 2) * -1;
-			var blur = 'blur(' + score + 'px)';
+	if (score < 0) {
+		score = (score - 2) * -1;
+		var blur = 'blur(' + score + 'px)';
+		$(children).css({ 'opacity': '0.25', '-webkit-filter': blur });
+		$(parent).hover(function () {
+			$(children).css({ 'opacity': '1', '-webkit-filter': 'blur(0)', '-webkit-transition': '-webkit-filter 1000ms linear, opacity 1000ms linear' });
+		}, function () {
 			$(children).css({ 'opacity': '0.25', '-webkit-filter': blur });
-			$(parent).hover(function () {
-				$(children).css({ 'opacity': '1', '-webkit-filter': 'blur(0)', '-webkit-transition': '-webkit-filter 1000ms linear, opacity 1000ms linear' });
-			}, function () {
-				$(children).css({ 'opacity': '0.25', '-webkit-filter': blur });
-			});
-		}
+		});
+	}
 }
 
 function tweetblur() {
@@ -60,36 +87,15 @@ function fbblur() {
 		var comment = $(this).find('.UFICommentBody').text();
 		var score;
 
-		analyzeAndCss(comment, this, this)
-		
+		analyzeAndCss(comment, this, this);
 	});
 
 	$('.userContentWrapper ').each(function () {
 		var post = $(this).find('.userContent p');
-
 		var postText = post.text();
 		var score;
 
 		analyzeAndCss(postText, post, post);
-
-		// retext.use(retextSentiment).use(function () {
-		// 	return function (tree) {
-		// 		if (tree.data) {
-		// 			score = tree.data.polarity;
-		// 		}
-		// 	};
-		// }).process(postText);
-
-		// if (score < 0) {
-		// 	score = (score - 2) * -1;
-		// 	var blur = 'blur(' + score + 'px)';
-		// 	$(post).css({ 'opacity': '0.25', '-webkit-filter': blur });
-		// 	$(post).hover(function () {
-		// 		$(post).css({ 'opacity': '1', '-webkit-filter': 'blur(0)', '-webkit-transition': '-webkit-filter 1000ms linear, opacity 1000ms linear' });
-		// 	}, function () {
-		// 		$(post).css({ 'opacity': '0.25', '-webkit-filter': blur });
-		// 	});
-		// }
 	});
 }
 
@@ -106,3 +112,9 @@ function unblur() {
 	});
 
 }
+
+
+// new Retext().use(sentiment, {
+//     'cat': -3,
+//     'dog': 3
+// });
--- extension/popup.js
+++ extension/popup.js
@@ -3,17 +3,27 @@ $('#toggle').change(function(){
 	console.log('click!');
 	if($(this).is(":checked")) {
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		  chrome.tabs.sendMessage(tabs[0].id, {command: "set_blur"}, function(response) {
-		    console.log('response?',response);
-		  });
+		  chrome.tabs.sendMessage(tabs[0].id, {command: "set_blur"});
 		});
 	}
   else {
   	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		  chrome.tabs.sendMessage(tabs[0].id, {command: "set_unblur"}, function(response) {
-		    console.log('response?',response);
-		  });
+		  chrome.tabs.sendMessage(tabs[0].id, {command: "set_unblur"});
 		});
   }
+});
 
+$("#polarityForm").submit(function(event){
+	var data = $(this).serializeArray();
+	event.preventDefault();
+	
+	chrome.storage.sync.get(null, function (storage){
+		var word = data[0].value;
+		storage[word] = data[1].value;
+		
+	  chrome.storage.sync.set(storage, function(){
+	  	console.log('stored!');
+			$(this).find("input").val("");
+	  });
+	});
 });
